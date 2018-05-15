300b9e3da3fd62650176b4700f8651fe203faca2
imharrywu <imharrywu@gmail.com>
2018-05-15 19:44:46 +0800

    add ref impl.

--- cntest.c
+++ cntest.c
@@ -7,10 +7,11 @@ int main(int argc, char * argv[])
 {
 	unsigned char hash[128];
 	int i = 0;
-	cryptonight_hash(hash, "123123123", 9);
+	cryptonight_hash(hash, "This is a test", 14);
 	for (i = 0; i < 32; i++)printf("%02x", hash[i]);
 	printf("\n");	
 	return 0;
 }
 
 // gcc -o cntest -g cntest.c -L./build/lib -lcryptonight
+// ref: https://cryptonote.org/cns/cns008.txt
